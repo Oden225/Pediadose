e7fc1fd11f9cfa8991e95ddc96b7a5df6c833ff3
Oden225 <131435571+Oden225@users.noreply.github.com>
2026-07-05 20:26:20 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pediadose-v4";
+const CACHE = "pediadose-v6";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
